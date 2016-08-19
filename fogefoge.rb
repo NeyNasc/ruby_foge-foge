cce531fe47cb8b01af4655d6e08e8484cf2efd42
Claudiney Nascimento <claudineydelima@gmail.com>
2016-08-19 20:29:33 -0300

    Adcionando movimentos laterais aos fantasmas

--- fogefoge.rb
+++ fogefoge.rb
@@ -1,7 +1,7 @@
 require_relative "ui"
 
 def joga(nome)
-	mapa = le_mapa(1)
+	mapa = le_mapa(2)
 
 	while true
 		desenha mapa
@@ -13,24 +13,25 @@ def joga(nome)
 			next
 		end
 
+
 		mapa[heroi[0]][heroi[1]] = " "
 		mapa[nova_posicao[0]][nova_posicao[1]] = "H"
+		move_fantasmas mapa
 	end	
 end
 
 def calculando_nova_posicao(heroi, direcao)
 	heroi = heroi.dup
-	case direcao
-		when "W"
-			heroi[0] -= 1
-		when "S"
-			heroi[0] += 1
-		when "A"
-			heroi[1] -= 1
-		when "D"
-			heroi[1] += 1
-	end	
-		heroi
+	movimentos = {
+		"W" => [-1, 0],
+		"S" => [+1, 0],
+		"A" => [0, -1],
+		"D" => [0, +1]
+	}	
+	movimento = movimentos[direcao]
+	heroi[0] += movimento[0]
+	heroi[1] += movimento[1]
+	heroi
 end
 
 def inicia_fogefoge
@@ -44,9 +45,6 @@ def le_mapa(numero)
 	mapa = texto.split("\n")
 end
 
-
-
-
 def encontra_jogador(mapa)
 	caracter_do_heroi = "H"
 	mapa.each_with_index do |linha_atual, linha|
@@ -57,8 +55,6 @@ def encontra_jogador(mapa)
 	end
 	#não encontrei
 end
-
-
 
 def posicao_valida?(mapa, posicao)
 	linhas = mapa.size
@@ -71,9 +67,27 @@ def posicao_valida?(mapa, posicao)
 	if estourou_linha || estourou_coluna
 		return false
 	end
-	#Isso pode dar erro!<<<<< Warning >>>>>
 	if mapa[posicao[0]][posicao[1]] == "X"
 		return false
 	end
 	true
+end
+
+def move_fantasmas(mapa)
+	caracter_do_fantasma = "F"
+	mapa.each_with_index do |linha_atual, linha|
+		linha_atual.chars.each_with_index do |caracter_atual, coluna|
+			eh_fantasma = caracter_atual == caracter_do_fantasma
+			if eh_fantasma
+				move_fantasma mapa,linha,coluna			
+			end
+		end
+	end	
+end
+
+def move_fantasma(mapa, linha, coluna)
+	mapa[linha][coluna] = " "
+	linha += 0
+	coluna += 1	
+	mapa[linha][coluna] = "F"
 end
