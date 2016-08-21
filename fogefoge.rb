fd2aac85344189bb3a55675d1f234d188ffd57be
Claudiney Nascimento <claudineydelima@gmail.com>
2016-08-21 17:24:13 -0300

    Adicionando conceitos de classes no jogo

--- fogefoge.rb
+++ fogefoge.rb
@@ -1,28 +1,29 @@
 require_relative "ui"
 require_relative "heroi"
 
+
+
 def joga(nome)
-	mapa = le_mapa(2)
+    mapa = le_mapa(2)
+    while true
+        desenha mapa
+        direcao = pede_movimento
 
-	while true
-		desenha mapa
-		direcao = pede_movimento
+        heroi = encontra_jogador mapa
+        nova_posicao = heroi.calcula_nova_posicao direcao
+        if !posicao_valida? mapa, nova_posicao.to_array
+            next
+        end
 
-		heroi = encontra_jogador mapa
-		nova_posicao = heroi.calculando_nova_posicao direcao
-		if !posicao_valida? mapa, nova_posicao.to_array
-			next
-		end
+        mapa[heroi.linha][heroi.coluna] = " "
+        mapa[nova_posicao.linha][nova_posicao.coluna] = "H"
 
-		mapa[heroi.linha][heroi.coluna] = " "
-		mapa[nova_posicao[0]][nova_posicao[1]] = "H"
-		mapa = move_fantasmas mapa
-
-		if jogador_perdeu?(mapa)
-			game_over
-			break			
-		end
-	end	
+        mapa = move_fantasmas mapa
+        if jogador_perdeu?(mapa)
+            game_over
+            break
+        end
+    end
 end
 
 def jogador_perdeu?(mapa)
