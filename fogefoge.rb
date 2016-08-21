b82ce69873aaa0f66218bc0b84798e67d26eb5a4
Claudiney Nascimento <claudineydelima@gmail.com>
2016-08-20 23:08:56 -0300

    Refatornado o metodo de posições válidas e aplicando posições aleatŕoias ao fantasma

--- fogefoge.rb
+++ fogefoge.rb
@@ -95,35 +95,33 @@ def move_fantasma(mapa, novo_mapa, linha, coluna)
 	posicoes = posicoes_validas_a_partir_de mapa, novo_mapa, [linha, coluna]
 
 	return if posicoes.empty?
-
-	posicao = posicoes[0]
+	aleatoria = rand posicoes.size
+	posicao = posicoes[aleatoria]
 
 	mapa[linha][coluna] = " "
 	novo_mapa[posicao[0]][posicao[1]] = "F"
 
 end
 
+
+def copia_mapa(mapa)
+	novo_mapa = mapa.join("\n").tr("F", " ").split("\n")
+end
+
+
 def posicoes_validas_a_partir_de(mapa, novo_mapa, posicao)
     posicoes = []
-    baixo = [posicao[0] + 1, posicao[1]]
-    if posicao_valida?(mapa, baixo) && posicao_valida?(novo_mapa, baixo)
-        posicoes << baixo
-    end
-    direita = [posicao[0], posicao[1] + 1]
-    if posicao_valida?(mapa, direita) && posicao_valida?(novo_mapa, direita)
-        posicoes << direita
-    end
-    cima = [posicao[0] - 1, posicao[1]]
-    if posicao_valida?(mapa, cima) && posicao_valida?(novo_mapa, cima)
-        posicoes << cima
-    end
-    esquerda = [posicao[0], posicao[1] - 1]
-    if posicao_valida?(mapa, esquerda) && posicao_valida?(novo_mapa, esquerda)
-        posicoes << esquerda
+    movimentos = [[-1,0],[0,+1],[+1,0],[0,-1]]
+
+    movimentos.each do |movimento|
+    	nova_posicao = soma posicao, movimento
+    	if posicao_valida?(mapa, nova_posicao) && posicao_valida?(novo_mapa, nova_posicao)
+			posicoes<<nova_posicao    		
+    	end
     end
     posicoes
 end
 
-def copia_mapa(mapa)
-	novo_mapa = mapa.join("\n").tr("F", " ").split("\n")
+def soma(vetor1, vetor2)	
+	[vetor1[0] + vetor2[0], vetor1[1] + vetor2[1]]
 end
